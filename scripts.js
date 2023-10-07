5114060e6cab036fc3aa1c39739f9dbf5454d884
Victor Gimeno <soliakt4@gmail.com>
2023-10-07 20:07:04 +0200

    funciona reset pero no las vidas

--- scripts.js
+++ scripts.js
@@ -10,14 +10,13 @@ window.onload = function () {
     // Añadir event listener del botón inicial
     const playButton = document.getElementById('playButton');
     playButton.addEventListener('click', function(){
-        generateGameContent();
         showGameContents();
         recorreIDS();
     });
 
     function generateGameContent() {
         // Definimos debajo de qué elemento vamos a generar la tabla (parentElement)
-        const parentElement = document.getElementById('gameContent__letters__lettersThemselves');
+        parentElement = document.getElementById('gameContent__letters__lettersThemselves');
         const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     
         const table = document.createElement('table');
@@ -45,27 +44,23 @@ window.onload = function () {
     function showGameContents() {
         document.getElementById('playButton').style.display = "none";
         document.getElementById('initialContent').style.display = "none";
+        resetGame();
+        arrayWordInProgress = [];
         document.getElementById('gameContent').style.display = "flex";
     }
-    
- 
+
+
     var letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var totalLetras = letras.length;
+    var remainingLives;
 
-    var remainingLives = 6;
-    var conjuntoDePalabras = ["hamburguesa", "arenal", "meteorito"];
-    for (var i = 0; i < conjuntoDePalabras.length; i++) var wordToDiscover = conjuntoDePalabras[Math.floor(Math.random() * conjuntoDePalabras.length)];
-    var wordInDiscovery = "";
     var wordLenght = wordToDiscover.length;
-    var availableLetters = wordToDiscover.length;
-    var arrayWordInProgress = [];
+
+
     var correctLetter = false;
 
+
     function recorreIDS(){
-        // Esto recorre todos los id y en caso de que algun evento se haya producido llama a la funcion letterClicked
-        for (var i = 0; i < totalLetras; i++) document.getElementById(letras[i]).addEventListener('click', letterClicked);
-        for (var i = 0; i < wordLenght; i++) arrayWordInProgress.push("_ ");
-        for (var f = 0; f < wordLenght; f++) wordInDiscovery += arrayWordInProgress[f];
 
         // Obtener el elemento del texto (h4) que vamos a sustituir por nuestra palabra oculta y por nuestras vidas
         const wordInHTML = document.getElementById('hiddenWord');
@@ -74,38 +69,40 @@ window.onload = function () {
         document.getElementById(remainingLives).style.display = "flex"
 
         // Reemplazarlos
-        livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
-        wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
+        //livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
+        //wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
 
     }
  
-    function letterClicked() {
+    function letterClicked(wordLength, id, availableLetters, arrayWordInProgress, wordInDiscovery) {
         correctLetter = false;
         wordInDiscovery = "";
-        for (var p = 0; p < wordLenght; p++) {
-            if (this.id == wordToDiscover[p]) {
+        // Va letra a letra comprobando el string de la palabra a descubrir a ver si coincide alguna letra con la introducida
+        for (var p = 0; p < wordLength; p++) {
+            if (id == wordToDiscover[p]) {
                 correctLetter = true;
                 availableLetters--;
-                for (var m = 0; m < wordLenght; m++) {
-                    if (m == p) arrayWordInProgress[m] = event.target.id;
+                // Va al array de las letras a descubrir y guarda en la posicion exacta de la palabra, la letra
+                for (var m = 0; m < wordLength; m++) {
+                    if (m == p) arrayWordInProgress[m] = id;
                 }
             }
+            // Cambia los colores de los botones
             if (correctLetter) event.target.style.backgroundColor = "green";
             else event.target.style.backgroundColor = "red";
             event.target.style.color = "white";
         }
-        for (var f = 0; f < wordLenght; f++) wordInDiscovery += arrayWordInProgress[f];
-        if (correctLetter == false) remainingLives--
-        else if (availableLetters == 0) gamePassedPopUp();
-        if (remainingLives < 1) gameOverPopUp();
-
+        for (var f = 0; f < wordLengthBueno; f++) wordInDiscovery += arrayWordInProgress[f];
         // Obtener el elemento del texto (h4) que vamos a sustituir por nuestra palabra oculta
-        const wordInHTML = document.getElementById('hiddenWord');
-        const livesInHTML = document.getElementById('availableLives');
-        
+        wordInHTML = document.getElementById('hiddenWord');
+        livesInHTML = document.getElementById('availableLives');
         // Reemplazarlo
         livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
         wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
+        if (correctLetter == false) remainingLives--;
+        else if (availableLetters == 0) gamePassedPopUp();
+        if (remainingLives < 1) gameOverPopUp();
+
 
         if(remainingLives > 5) document.getElementById(remainingLives).style.display = "flex";
 
@@ -114,54 +111,74 @@ window.onload = function () {
             document.getElementById(remainingLives).style.display = "flex"
         }
     }
-}
-/*
-function resetValues(){
-    remainingLives = 6;
-    wordToDiscover = conjuntoDePalabras[Math.floor(Math.random() * conjuntoDePalabras.length)];
-    wordLenght = wordToDiscover.length;
-    availableLetters = wordToDiscover.length;
-    arrayWordInProgress = [];
-    correctLetter = false;
+    function resetGame() {
+        remainingLives = 6;
+        var conjuntoDePalabras = ["hamburguesa", "arenal", "meteorito"];
+        wordToDiscover = conjuntoDePalabras[Math.floor(Math.random() * conjuntoDePalabras.length)];
+        wordLengthBueno = wordToDiscover.length;
+        var availableLetters = wordLengthBueno;
 
-    for (var i = 0; i < totalLetras; i++) document.getElementById(letras[i]).addEventListener('click', letterClicked);
-
-    for (var i = 0; i < wordLenght; i++) arrayWordInProgress.push("_ ");
-
-    wordInDiscovery = arrayWordInProgress.join('');
-    const wordInHTML = document.getElementById('hiddenWord');
-    const livesInHTML = document.getElementById('availableLives');
-
-    document.getElementById(remainingLives).style.display = "flex";
-    livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
-    wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
-}
-*/
-
-function gameOverPopUp() {
-    Swal.fire({
-        title: 'Has perdido amigo mío',
-        text: '¿Quieres jugar otra partida?' ,
-        icon: 'error', 
-        confirmButtonText: 'Jugar otra partida'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            location.reload();
+    
+        var arrayWordInProgress = [];
+        correctLetter = false;
+        const parentElement = document.getElementById('gameContent__letters__lettersThemselves');
+        parentElement.innerHTML = '';
+        var wordInDiscovery = "";
+    
+        // Generar la interfaz del juego
+        generateGameContent();
+        
+        // Restablecer el texto de vidas y palabra oculta
+        const wordInHTML = document.getElementById('hiddenWord');
+        const livesInHTML = document.getElementById('availableLives');
+        livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
+        // Rellenamos cada letra del array con _
+        for (var i = 0; i < wordLengthBueno; i++) {
+            arrayWordInProgress.push("_ ");
         }
-    });
+
+        // Aqui creamos la palabra vacía con los _ para mostrarlo en el html
+        for (var f = 0; f < wordLengthBueno; f++) wordInDiscovery += arrayWordInProgress[f];
+        wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
+
+
+        // Hasta aqui esta bien
+        // Esto recorre todos los id y en caso de que algun evento se haya producido llama a la funcion letterClicked
+        for (var i = 0; i < totalLetras; i++) document.getElementById(letras[i]).addEventListener('click', function(){
+            letterClicked(wordLengthBueno, this.id, availableLetters, arrayWordInProgress, wordInDiscovery)
+        });
+        
+    }
+    
+    function gameOverPopUp() {
+        Swal.fire({
+            title: 'Has perdido amigo mío',
+            text: '¿Quieres jugar otra partida?' ,
+            icon: 'error', 
+            confirmButtonText: 'Jugar otra partida'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resetGame();
+            }
+        });
+    }
+    
+    function gamePassedPopUp() {
+        Swal.fire({
+            title: '¡Has ganado!',
+            text: '¿Quieres jugar otra partida?',
+            icon: 'success',
+            confirmButtonText: 'Jugar otra partida'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resetGame();
+            }
+        });
+    }
+   
+    
 }
 
-function gamePassedPopUp() {
-    Swal.fire({
-        title: '¡Has ganado!',
-        text: '¿Quieres jugar otra partida?',
-        icon: 'success',
-        confirmButtonText: 'Jugar otra partida'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            location.reload();
-        }
-    });
-}
+
 
 
