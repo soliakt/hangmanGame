e309d9a3cdb7b2ecc1ae2a4cf9cb8256c1488bf7
Victor Gimeno <soliakt4@gmail.com>
2023-10-09 18:10:37 +0200

    keyboard input working

--- scripts.js
+++ scripts.js
@@ -3,13 +3,14 @@
     NIA: 10017766
     Práctica: Juego del ahorcado
 */
-
-
-
 window.onload = function () {
     // Añadir event listener del botón inicial
-    const playButton = document.getElementById('playButton');
-    playButton.addEventListener('click', showGameContents);
+    const playButton1 = document.getElementById('playButton1');
+    playButton1.addEventListener('click', showGameContents);
+    const playButton2 = document.getElementById('playButton2');
+    playButton2.addEventListener('click', showGameContents);
+    const infoButton = document.getElementById('infoButton');
+    infoButton.addEventListener('click', showGameInstructions);
 
     function generateGameContent() {
         // Definimos debajo de qué elemento vamos a generar la tabla (parentElement)
@@ -36,66 +37,73 @@ window.onload = function () {
         parentElement.appendChild(table);
     }
     
-
-    
     function showGameContents() {
-        document.getElementById('playButton').style.display = "none";
+        document.getElementById('playButton1').style.display = "none";
+        document.getElementById('playButton2').style.display = "none";
         document.getElementById('initialContent').style.display = "none";
+        document.getElementById('infoContent').style.display = "none";
         resetGame();
         arrayWordInProgress = [];
         document.getElementById('gameContent').style.display = "flex";
     }
 
+    function showGameInstructions(){
+        document.getElementById('playButton1').style.display = "none";
+        document.getElementById('playButton2').style.display = "flex";
+        document.getElementById('initialContent').style.display = "none";
+        document.getElementById('infoContent').style.display = "flex";
+    }
+
     var remainingLives;
     let availableLetters; 
 
-
-    function letterClicked(wordLength, id, arrayWordInProgress, wordInDiscovery) {
+    function letterClicked(wordLength, id, arrayWordInProgress, wordInDiscovery, attemptedLetters) {
         var correctLetter = false;
-        wordInDiscovery = "";
-        // Va letra a letra comprobando el string de la palabra a descubrir a ver si coincide alguna letra con la introducida
-        for (var p = 0; p < wordLength; p++) {
-            if (id == wordToDiscover[p]) {
-                correctLetter = true;
-                availableLetters--;
-                // Va al array de las letras a descubrir y guarda en la posicion exacta de la palabra, la letra
-                for (var m = 0; m < wordLength; m++) {
-                    if (m == p) arrayWordInProgress[m] = id;
+        if (!attemptedLetters.includes(id)) {
+            attemptedLetters.push(id);
+            wordInDiscovery = "";
+            // Va letra a letra comprobando el string de la palabra a descubrir a ver si coincide alguna letra con la introducida
+            for (var p = 0; p < wordLength; p++) {
+                if (id[0] === wordToDiscover[p]) {
+                    correctLetter = true;
+                    availableLetters--;
+                    // Va al array de las letras a descubrir y guarda en la posicion exacta de la palabra, la letra
+                    for (var m = 0; m < wordLength; m++) {
+                        if (m == p) arrayWordInProgress[m] = id;
+                    }
                 }
+                // Cambia los colores de los botones
+                if (correctLetter) document.getElementById(id).style.backgroundColor = "green";
+                else document.getElementById(id).style.backgroundColor = "red";
+                document.getElementById(id).style.color = "white";
             }
-            // Cambia los colores de los botones
-            if (correctLetter) event.target.style.backgroundColor = "green";
-            else event.target.style.backgroundColor = "red";
-            event.target.style.color = "white";
+            for (var f = 0; f < wordLengthBueno; f++) wordInDiscovery += arrayWordInProgress[f];
+            if (correctLetter == false) remainingLives--;
+            // Obtener el elemento del texto (h4) que vamos a sustituir por nuestra palabra oculta
+            var wordInHTML = document.getElementById('hiddenWord');
+            var livesInHTML = document.getElementById('availableLives');
+            // Reemplazarlo
+            livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
+            wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
+            if (remainingLives < 1) gameOverPopUp(); 
+            else if(remainingLives > 5) {
+                document.getElementById(remainingLives).style.display = "flex";
+                document.getElementById(0).style.display = "none";
+            }
+            else {
+                document.getElementById(remainingLives + 1).style.display = "none"
+                document.getElementById(remainingLives).style.display = "flex"
+            }
+            if (availableLetters == 0) gamePassedPopUp();
         }
-        for (var f = 0; f < wordLengthBueno; f++) wordInDiscovery += arrayWordInProgress[f];
-        if (correctLetter == false) remainingLives--;
-        // Obtener el elemento del texto (h4) que vamos a sustituir por nuestra palabra oculta
-        var wordInHTML = document.getElementById('hiddenWord');
-        var livesInHTML = document.getElementById('availableLives');
-        // Reemplazarlo
-        livesInHTML.innerHTML = "Vidas restantes: " + remainingLives + "\n";
-        wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
-        if (remainingLives < 1) gameOverPopUp(); 
-        else if(remainingLives > 5) {
-            document.getElementById(remainingLives).style.display = "flex";
-            document.getElementById(0).style.display = "none";
-        }
-        else {
-            document.getElementById(remainingLives + 1).style.display = "none"
-            document.getElementById(remainingLives).style.display = "flex"
-        }
-        if (availableLetters == 0) gamePassedPopUp();
-
     }
     function resetGame() {
+        var attemptedLetters = []
         var letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         var totalLetras = letras.length;
         remainingLives = 6;
         document.getElementById(remainingLives).style.display = "flex";
         for (i = 0; i < 6; i++) document.getElementById(i).style.display = "none";
-
-        
         var conjuntoDePalabras = ["hamburguesa", "arenal", "meteorito"];
         wordToDiscover = conjuntoDePalabras[Math.floor(Math.random() * conjuntoDePalabras.length)];
         wordLengthBueno = wordToDiscover.length;
@@ -123,16 +131,17 @@ window.onload = function () {
         for (var f = 0; f < wordLengthBueno; f++) wordInDiscovery += arrayWordInProgress[f];
         wordInHTML.innerHTML = wordInDiscovery.toUpperCase();
 
-
-        // Hasta aqui esta bien
         // Esto recorre todos los id y en caso de que algun evento se haya producido llama a la funcion letterClicked
         for (var i = 0; i < totalLetras; i++) document.getElementById(letras[i]).addEventListener('click', function(){
-            letterClicked(wordLengthBueno, this.id, arrayWordInProgress, wordInDiscovery)
+            letterClicked(wordLengthBueno, this.id, arrayWordInProgress, wordInDiscovery, attemptedLetters)
         });
 
         window.addEventListener('keydown', function(event) {
-            const key = event.key;
-            letterClicked(wordLengthBueno, key, availableLetters, arrayWordInProgress, wordInDiscovery);
+            const key = event.key.toLowerCase();
+            // Esto evita incorporar teclas que no sean letras
+            if (letras.includes(key)) {
+                letterClicked(wordLengthBueno, key, arrayWordInProgress, wordInDiscovery, attemptedLetters);
+            }
         });
         
     }
@@ -161,11 +170,5 @@ window.onload = function () {
                 resetGame();
             }
         });
-    }
-   
-    
+    }   
 }
-
-
-
-
